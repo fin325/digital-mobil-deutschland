2af5e965d3a73867c11ecf4e2a790f150433f38c
fin325 <finevich325@gmail.com>
2026-04-02 14:47:28 +0200

    main.js erstellen

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/* === main.js — Инициализация === */
+
+document.addEventListener('DOMContentLoaded', () => {
+    initClock();   // clock.js
+    getWeather();  // weather.js
+    loadNews();    // news.js
+});

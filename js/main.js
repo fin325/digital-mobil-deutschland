7dc2b56d1771fc5231f0443b32b80c475514329a
fin325 <finevich325@gmail.com>
2026-04-04 23:58:38 +0200

    main.js aktualisieren

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Фоновый пинг для пробуждения Render-приложения при загрузке сайта
 fetch('https://pdf-compressor-web.onrender.com/wakeup', { mode: 'no-cors' })
+  .then(() => alert('Сигнал на Render отправлен!'))
   .catch(() => {});
 
 // ... дальше идет ваш обычный код из main.js ...

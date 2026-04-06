a61dc17ca927e81b501e1e591a18d8e8ce3a86e0
fin325 <finevich325@gmail.com>
2026-04-06 21:37:15 +0200

    components.js aktualisieren

--- js/components.js
+++ js/components.js
@@ -46,3 +46,22 @@ class AppHeader extends HTMLElement {
 }
 customElements.define('app-header', AppHeader);
 
+class ScrollHint extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="scroll-hint-container">
+                <div class="scroll-hint-left">
+                    <div class="swipe-finger-wrapper">
+                        <div class="swipe-finger">👇🏼</div>
+                    </div>
+                    <span class="scroll-hint-text">Свайп меню</span>
+                </div>
+                <div class="scroll-arrows">
+                    <button class="arrow-btn" onclick="scrollTabs(-1)">←</button>
+                    <button class="arrow-btn" onclick="scrollTabs(1)">→</button>
+                </div>
+            </div>
+        `;
+    }
+}
+

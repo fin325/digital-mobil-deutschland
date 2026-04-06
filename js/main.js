20c4ea36b6117ca1cba7e10683951f4b846851f3
fin325 <finevich325@gmail.com>
2026-04-06 23:24:02 +0200

    main.js aktualisieren

--- js/main.js
+++ js/main.js
@@ -31,3 +31,22 @@ document.addEventListener('DOMContentLoaded', () => {
 // Фоновый пинг для пробуждения Render-приложения при загрузке сайта
 fetch('https://ВАШ_АДРЕС_НА_RENDER.onrender.com/wakeup', { mode: 'no-cors' })
   .catch(() => {});
+
+function loadVideo(el, id) {
+    el.innerHTML = `
+        <iframe width="100%" height="315"
+        src="https://www.youtube-nocookie.com/embed/${id}?autoplay=1&rel=0"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen></iframe>
+    `;
+}
+
+function loadPlaylist(el, listId) {
+    el.innerHTML = `
+        <iframe width="100%" height="315"
+        src="https://www.youtube-nocookie.com/embed/videoseries?list=${listId}"
+        frameborder="0"
+        allowfullscreen></iframe>
+    `;
+}

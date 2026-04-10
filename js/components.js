cd921fdd3400d4270cc4796e43c8f6b72fab70f3
fin325 <finevich325@gmail.com>
2026-04-10 13:46:21 +0200

    components.js aktualisieren

--- js/components.js
+++ js/components.js
@@ -47,6 +47,7 @@ class AppHeader extends HTMLElement {
                 <div class="site-header">
                     <div class="site-title">Digital & Mobil in Deutschland</div>
                     <div class="site-subtitle">Stadt Hattingen</div>
+                    <a href="/ru/" class="lang-btn">🇷🇺 RU</a>
                 </div>
         `;
     }

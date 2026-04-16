be6a90aa7e6ae4336bc92acc40b78f9e76461026
fin325 <finevich325@gmail.com>
2026-04-16 22:44:28 +0200

    weather.js aktualisieren

--- js/weather.js
+++ js/weather.js
@@ -144,7 +144,7 @@ function updateAQIUI(index) {
 
     switch (index) {
         case 1: color = "#2ecc71"; iconClass = "icon-leaf";    break;
-        case 2: color = "#f1c40f"; iconClass = "icon-wond";    break;
+        case 2: color = "#f1c40f"; iconClass = "icon-wind";    break;
         case 3: color = "#e67e22"; iconClass = "icon-fog";     break;
         case 4: color = "#e74c3c"; iconClass = "icon-warning"; break;
         case 5: color = "#9b59b6"; iconClass = "icon-mask";    break;

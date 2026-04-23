f583011f16c80cdf55ada80f97728e9dee2c066d
fin325 <finevich325@gmail.com>
2026-04-24 00:17:45 +0200

    components.js aktualisieren

--- js/components.js
+++ js/components.js
@@ -148,6 +148,11 @@ class AppHeader extends HTMLElement {
                     <div class="site-title">Digital & Mobil in Deutschland</div>
                     <div class="site-subtitle">${texts.city}</div>
                 </div>
+                <div id="eur-widget" class="eur-rate-widget" title="Офіційний курс НБУ">
+        <span class="eur-flag">🇪🇺</span>
+        <span class="eur-label">EUR / UAH</span>
+        <span class="eur-value" id="eur-value">…</span>
+                </div>
                 <a href="${langHref}" class="lang-btn">${langLabel}</a>
             </div>
         `;
